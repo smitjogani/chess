9282e62becde9b5e6018f64f2ce60911c9d704e6
smit <smitjogani00@gmail.com>
2024-05-28 12:13:00 +0530

    devlopment

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(path.join(__dirname, "public"));
