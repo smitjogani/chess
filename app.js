322c8a11134f9810c1fad1867c13f8b2bc65d9d5
smit <smitjogani00@gmail.com>
2024-05-28 16:19:28 +0530

    backend

--- app.js
+++ app.js
@@ -23,6 +23,44 @@ app.get("/", (req, res) => {
 
 io.on("connection", (uniqueSocket) => {
   console.log("connected");
+
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
+
+  uniqueSocket.on("disconnect", () => {
+    if (uniqueSocket.id === players.white) {
+      delete players.white;
+    } else if (uniqueSocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniqueSocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniqueSocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move : ", move);
+        uniqueSocket.emit("Invalid move", move);
+      }
+    } catch (error) {
+      console.log(error);
+      uniqueSocket.emit("Invalid move : ", error);
+    }
+  });
 });
 
 server.listen(3000, () => {
